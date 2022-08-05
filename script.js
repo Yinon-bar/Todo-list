c2233b7912fceff1ae93a22c096cf46d93b91c60
Yinon-bar <63056754+Yinon-bar@users.noreply.github.com>
2022-08-06 01:00:55 +0300

    Task Manager - Authantication & delete all btn

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // מאפיינים גלובלים
 
-const taskName = document.querySelector("#task-name");
-const taskDate = document.querySelector("#task-date");
-const taskTime = document.querySelector("#task-time");
-const noteSection = document.querySelector("#notes_section");
+let taskName = document.querySelector("#task-name");
+let taskDate = document.querySelector("#task-date");
+let taskTime = document.querySelector("#task-time");
+let noteSection = document.querySelector("#notes_section");
 
 // יצירת מערך גלובלי
 let taskArray = [];
@@ -21,35 +21,58 @@ function getTasks(arg) {
       taskArray.push(task);
       renderElement(task, arg);
     }
+  } else {
+    noteSection.innerHTML = "";
   }
 }
 
+// יצירת משימות חדשות
 function createTask() {
   // מניעה של שליחת הטופס
   event.preventDefault();
 
-  // איסוף של כל המידע לאובייקט אחד
-  const taskObj = {
-    id: taskArray.length,
-    task: taskName.value,
-    date: taskDate.value,
-    time: taskTime.value,
-  };
-  console.log(taskObj);
-  taskArray.push(taskObj);
-  console.log(taskArray);
+  // בדיקת נתונים
+  function auth() {
+    if (taskName.value == "" || taskDate.value == "" || taskTime.value == "") {
+      alert("אנא מלא את כל השדות");
+      return;
+    } else {
+      // רק במידה והשדות מלאים ניתן להמשיך
+      // איסוף של כל המידע לאובייקט אחד
+      const taskObj = {
+        id: taskArray.length,
+        task: taskName.value,
+        date: taskDate.value,
+        time: taskTime.value,
+      };
+      console.log(taskObj);
+      taskArray.push(taskObj);
+      console.log(taskArray);
 
-  renderElement(taskObj);
-  // counter++;
-  saveToLocal(taskArray);
+      renderElement(taskObj);
+      saveToLocal(taskArray);
+    }
+  }
+  auth();
+}
+
+function resetForm() {
+  let form = document.querySelector("#form");
+  form.reset();
+}
+
+function manualReset() {
+  taskName.value = "";
+  taskDate.value = "";
+  taskTime.value = "";
 }
 
 // שמירת המערך אזורית
-function saveToLocal(arr) {
-  localStorage.setItem("tasks", JSON.stringify(arr));
+function saveToLocal(taskArr) {
+  localStorage.setItem("tasks", JSON.stringify(taskArr));
 }
 
-function renderElement(obj, arg = "") {
+function renderElement(obj = "", arg = "") {
   noteSection.innerHTML += `
   <div class="note anim" id="note_${obj.id}" onmouseenter="trashHover(${obj.id})" onmouseleave="trashHover(${obj.id})">
   <div class="task-content">
@@ -62,21 +85,18 @@ function renderElement(obj, arg = "") {
   <div class="time">${obj.time}</div>
   </div>
   `;
+
   // בדיקה האם לבצע את האנימציה
+  // הצעה של ירון לעשות קוורי סלקטור לכולם ולא לאחד
+  let animDiv = document.querySelector(".anim");
   if (arg) {
-    let animDiv = document.querySelector(".anim");
     animDiv.classList.remove("anim");
   } else {
     setTimeout(function () {
-      let animDiv = document.querySelector(".anim");
       animDiv.classList.remove("anim");
+      manualReset();
     }, 400);
   }
-}
-
-function resetForm() {
-  const form = document.querySelector("#form");
-  form.reset();
 }
 
 // לחצן מחיקה
@@ -93,12 +113,17 @@ function trashHover(arg) {
 }
 
 function deleteTask(objId) {
-  // location.reload();
-  // localStorage.clear();
   taskArray.splice(objId, 1);
   console.log(taskArray);
   saveToLocal(taskArray);
   // יש לקרוא לפונקציה שבודקת האם יש משימות ב"זכרון" ולרנדר אותם
   // ןלהכניס לה ארגומנט כדי שהפונקציה תדע שהיא הגיעה מכאן
   getTasks("doNotAnim");
+}
+
+function deleteAll() {
+  event.preventDefault();
+  taskArray = [];
+  localStorage.clear();
+  getTasks();
 }
